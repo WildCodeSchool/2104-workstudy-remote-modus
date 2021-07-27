c5cd3c49f105334529498ba1eaa051f3ec8781b6
Alex-Ljeune <a.roger37@gmail.com>
2021-07-27 16:48:15 +0200

    connected to graphQL

--- front/src/routes/askingHelpPosts/AskingHelpPosts.tsx
+++ front/src/routes/askingHelpPosts/AskingHelpPosts.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 import "../../css/styles.css";
+import { useQuery, gql } from "@apollo/client";
 import fakeDataPosts from "./FakeDataPosts";
 import PostContainer from "./PostContainer";
 
+const GETALLPOSTS = gql`
+  query GetAllPosts {
+    allPosts {
+      id
+      title
+      wysiwyg
+      skills {
+        value
+      }
+    }
+  }
+`;
+
 const AskingHelpPosts = (): JSX.Element => {
+  const { loading, error, data } = useQuery(GETALLPOSTS);
+  if (data !== undefined) {
+    console.log("coucou", data.allPosts);
+  }
   return (
     <>
       <h1>HELLO </h1>
