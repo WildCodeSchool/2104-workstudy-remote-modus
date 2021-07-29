82d0a5ceead7268fa1209afa690e3b7dd50b6d32
Alex-Ljeune <a.roger37@gmail.com>
2021-07-29 11:40:06 +0200

    styled HelpPosts

--- front/src/components/navbar/Navbar.tsx
+++ front/src/components/navbar/Navbar.tsx
@@ -5,16 +5,27 @@ import "./navbar.css";
 const Navbar = (): JSX.Element => {
   return (
     <div className="topnav">
-      <div>
-        <NavLink to="/" exact>
-          Home
-        </NavLink>
-        <NavLink to="/asking-help-form">AskingHelpForm</NavLink>
-        <NavLink to="/asking-help-index">AskingHelpPosts</NavLink>
+      <div style={{ display: "flex" }}>
+        <img
+          src="https://res.cloudinary.com/dykscnyvu/image/upload/v1627483699/Moddusey/logo1_cf1vfb.png"
+          alt="Logo"
+          className="logo"
+        />
+        <div>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
+          <NavLink to="/asking-help-form">AskingHelpForm</NavLink>
+          <NavLink to="/asking-help-index">AskingHelpPosts</NavLink>
+        </div>
       </div>
       <div className="welcomemessage">
         <div className="topnav message">Welcome user@mail.com</div>
-        <img src="user-solid.svg" alt="profileIcon" className="profileIcon" />
+        <img
+          src="https://res.cloudinary.com/dykscnyvu/image/upload/v1627483744/Moddusey/persona4_yhsdi3.png"
+          alt="profileIcon"
+          className="profileIcon"
+        />
       </div>
     </div>
   );
